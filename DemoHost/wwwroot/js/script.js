aa5405ee4081783345590207ea7aa312ba2df93f
Weavy Guru <rickard@weavy.com>
2022-05-02 10:41:53 +0200

    nisse

--- DemoHost/wwwroot/js/script.js
+++ DemoHost/wwwroot/js/script.js
@@ -1,1 +1,27 @@
-console.log("TODO: we need to add chat...");
+// fetch token for logged in user from backend
+var getToken = function () {
+    return fetch('/token').then(response => response.text());
+};
+
+// instantiate weavy
+var weavy = window.weavy = new Weavy({
+    url: "https://hansson.weavy.io",
+    jwt: getToken
+});
+
+// create app
+var messenger = weavy.app({
+    id: "my-messenger",
+    type: "messenger",
+    container: "#messenger-container",
+    open: false,
+    badge: true,
+})
+
+// toggle messenger
+document.getElementById("messenger-button").addEventListener("click", () => messenger.toggle());
+
+// handle markup changes
+messenger.on("app-open", () => document.getElementById("messenger-container").classList.add("open"))
+messenger.on("app-close", () => document.getElementById("messenger-container").classList.remove("open"));
+
